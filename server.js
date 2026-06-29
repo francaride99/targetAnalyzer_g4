51b5dd113be87c312d256c2a7a8c75ad8b547661
francaride99 <franciscocaride99@gmail.com>
2026-06-29 19:09:21 -0300

    Se agrega el robot al proyecto main, modificaciones en codigo ya que no dependemos de http requests para comunicarnos, integrado al proyecto de backend. Pendiente sumar frontend

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const path = require('path');
 const app = express(); //Creamos una constante con la carga de server express
 const PUERTO = 3001;
 
+const ejecutarExtraccion = require('./robot_mod');
+
 console.log('Estoy vivo!' + '\n');
 
 console.log('Desarrollo Backend grupo 4' + '\n');
@@ -33,7 +35,7 @@ function log(mensaje) {
 
 let tiempos = {};
 
-app.post('/api/escanear',(req, res)=>{ //Generamos una excepcion a POST
+app.post('/api/escanear', async (req, res) => { //Generamos una excepcion a POST, se agrega async
 //Tenemos 2 objetos generados para las requerst y response	
   const id = Date.now();
   tiempos[id] = {};
@@ -47,35 +49,61 @@ app.post('/api/escanear',(req, res)=>{ //Generamos una excepcion a POST
   tiempos[id].t2 = Date.now();
   log(`[T2] Backend → Robot: ${tiempos[id].t2} (Delay: ${tiempos[id].t2 - tiempos[id].t1}ms)`);
   
-  // Enviar al robot
-  fetch(`http://localhost:${PUERTO}/api/procesar`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ url: urlRecibida, id })
-  }).then(res => res.json()).then(data => {
-    log(`[RESPUESTA ROBOT - HTTP 200] ${JSON.stringify(data)}`);
-  }).catch(err => {
-    log(`[ERROR ROBOT] ${err.message}`); //Atrapamos el error en caso de quel robot falle por cuestiones de seguridad, etc
-  });
+  // // Enviar al robot PARTE VIEJA PEGANDO POR HTTP REQUESTs
+  // fetch(`http://localhost:${PUERTO}/api/procesar`, {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ url: urlRecibida, id })
+  // }).then(res => res.json()).then(data => {
+  //   log(`[RESPUESTA ROBOT - HTTP 200] ${JSON.stringify(data)}`);
+  // }).catch(err => {
+  //   log(`[ERROR ROBOT] ${err.message}`); //Atrapamos el error en caso de quel robot falle por cuestiones de seguridad, etc
+  // });
 
+  //NUEVA COMUNICACION CON ROBOT
+  try {
+
+      const resultado = await ejecutarExtraccion(urlRecibida);
+
+      tiempos[id].t3 = Date.now();
+      tiempos[id].t4 = Date.now();
+
+      log(`[T3] Robot finalizó: ${tiempos[id].t3}`);
+      log(`[T4] Backend recibió resultado: ${tiempos[id].t4}`);
+      log(`[DATOS ROBOT] ${JSON.stringify(resultado)}`);
+      log(`TIEMPO TOTAL: ${tiempos[id].t4 - tiempos[id].t1}ms`);
+      log(`${'─'.repeat(60)}`);
+
+      res.json(resultado);
+
+  } catch (err) {
+
+      log(`[ERROR ROBOT] ${err.message}`);
+
+      res.status(500).json({
+          estado: "ERROR",
+          mensaje: err.message
+      });
+
+  }
   res.json({ estado: 'EXITO', id });
 });
 
-// T4: Robot responde
-app.post('/api/respuesta', (req, res) => {
-  const { id, t3, datos } = req.body;
+// // T4: Robot responde TAMBIEN ES VERSION POR HTTP REQUESTs
+// app.post('/api/respuesta', (req, res) => {
+//   const { id, t3, datos } = req.body;
   
-  tiempos[id].t3 = t3;
-  tiempos[id].t4 = Date.now();
+//   tiempos[id].t3 = t3;
+//   tiempos[id].t4 = Date.now();
   
-  log(`[T3] Robot → Backend: ${t3}`);
-  log(`[T4] Backend recibió: ${tiempos[id].t4}`);
-  log(`[DATOS ROBOT] ${JSON.stringify(datos)}`); //Convertimos la data en string para 
-  log(`TIEMPO TOTAL: ${tiempos[id].t4 - tiempos[id].t1}ms`);
-  log(`${'─'.repeat(60)}\n`); //Separamos texto para saber cuando arranca y termina cada log
+//   log(`[T3] Robot → Backend: ${t3}`);
+//   log(`[T4] Backend recibió: ${tiempos[id].t4}`);
+//   log(`[DATOS ROBOT] ${JSON.stringify(datos)}`); //Convertimos la data en string para 
+//   log(`TIEMPO TOTAL: ${tiempos[id].t4 - tiempos[id].t1}ms`);
+//   log(`${'─'.repeat(60)}\n`); //Separamos texto para saber cuando arranca y termina cada log
   
-  res.json({ ok: true });
-});
+//   res.json({ ok: true });
+// });
 
 app.listen(PUERTO, () => {
   log(`[BUNKER CENTRAL] Escuchando en puerto ${PUERTO}`); //Constante 3000
